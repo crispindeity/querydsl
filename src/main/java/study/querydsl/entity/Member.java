c75366e938cd8dcd2effa12bc8cce25cc4869d36
geombong <shoy1415@gmail.com>
2022-10-18 14:01:44 +0900

    project setting

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,15 @@
+package study.querydsl.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+}
--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package study.querydsl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     void contextLoads() {
-    }
+        Member member = new Member();
+        em.persist(member);
 
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QMember qMember = QMember.member;
+
+        Member result = query
+                .selectFrom(qMember)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(member);
+    }
 }
