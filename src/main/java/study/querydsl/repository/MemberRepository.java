511ee63f2e21af35a29644f770dd4808654119bc
geombong <shoy1415@gmail.com>
2022-10-24 20:32:47 +0900

    Querydsl Predicate Executor

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -2,12 +2,13 @@ package study.querydsl.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
 import study.querydsl.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
     List<Member> findByUsername(String username);
 }
